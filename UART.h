002c253709a696c5786eba0dbd3bc2adff16f429
Alcaraz-Jesus <127097358+Alcaraz-Jesus@users.noreply.github.com>
2025-12-05 01:56:06 -0800

    Update UART.h

--- UART.h
+++ UART.h
@@ -1,25 +1,38 @@
-// Prototypes
-// Initialization
-UART_Ini(uint8_t com, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop)
+// UART.h 
+#ifndef UART_H
+#define UART_H
 
-// Send
+#include <stdint.h>
+
+// ---- Colores (indices al arreglo de UART.c) ----
+#define BLACK   0
+#define RED     1
+#define GREEN   2
+#define YELLOW  3
+#define BLUE    4
+#define MAGENTA 5
+#define CYAN    6
+#define WHITE   7
+
+// ---- Inicializacion ----
+void UART_Ini(uint8_t com, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop);
+
+// ---- Envio ----
 void UART_puts(uint8_t com, char *str);
 void UART_putchar(uint8_t com, char data);
 
-// Received
+// ---- Recepcion ----
 uint8_t UART_available(uint8_t com);
-char UART_getchar(uint8_t com );
-void UART_gets(uint8_t com, char *str);
+char    UART_getchar(uint8_t com);
+void    UART_gets(uint8_t com, char *str);
 
-// Escape sequences
-UART_clrscr( uint8_t com );
-UART_setColor(uint8_t com, uint8_t color);
-UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
+// ---- Secuencias ANSI ----
+void UART_clrscr(uint8_t com);
+void UART_setColor(uint8_t com, uint8_t color);
+void UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
 
-#define YELLOW  0 // Fixme 
-#define GREEN   0 // Fixme 
-#define BLUE    0 // Fixme 
+// ---- Utilidades ----
+void      itoa(uint16_t number, char* str, uint8_t base);
+uint16_t  atoi(char *str);
 
-// Utils
-void itoa(uint16_t number, char* str, uint8_t base) 
-uint16_t atoi(char *str)
+#endif // UART_H
